d9dca459649ee44aac79c529bfd020ad93da3a3b
Peter Styk <polfilm@gmail.com>
2016-06-22 03:41:15 +0100

    changes to security group and configure scripts. now launching. still no profile

--- cb-configure.sh
+++ cb-configure.sh
@@ -1,15 +1,21 @@
-wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt-get update
-sudo apt-get install jenkins openjdk-7-jdk git
+# Ubuntu Trusty
+# From Ubuntu user
+pushd /tmp
+    sudo wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+    sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+    sudo apt-get update
+    sudo apt-get install openjdk-7-jdk git jenkins -y
+    sudo groupadd hab && useradd -g hab -s /bin/bash -m hab
+    sudo usermod -aG docker jenkins
+    sudo usermod -aG docker hab
+    sudo curl -fsSL https://get.docker.com/ | sh
+    sudo curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+    sudo curl -sSf https://static.rust-lang.org/rustup.sh | sh
+popd
 
-curl -fsSL https://get.docker.com/ | sh
-curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-curl -sSf https://static.rust-lang.org/rustup.sh | sh
-usermod -aG docker jenkins
-usermod -aG docker hab
- 
-groupadd hab && useradd -g hab -d /home/hab -s /bin/bash -m hab
-pushd to ~/git (on jenkins user)
-git clone https://github.com/habitat-sh/habitat
-bash /var/lib/jenkins/git/habitat/components/hab/install.sh
+sudo su -c "mkdir -p /var/lib/jenkins/git/habitat" jenkins
+sudo su -c "git clone https://github.com/habitat-sh/habitat /var/lib/jenkins/git/habitat" jenkins
+sudo su -c "/var/lib/jenkins/git/habitat/components/hab/install.sh" 
+
+
+
