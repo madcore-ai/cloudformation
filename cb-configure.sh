7a39a7e75da660349b5d2b9944f18fd9aa98f137
Peter Styk <peter@styk.tv>
2016-06-30 14:57:38 +0100

    updated to java 8

--- cb-configure.sh
+++ cb-configure.sh
@@ -1,17 +1,18 @@
 #!/bin/bash -v
 # Ubuntu Trusty Initialization from Cloud-Init
 # From Ubuntu user
+# Maitained by Peter Styk (devopsfactory@styk.tv)
 
 # PREREQUESITES
 pushd /tmp
     sudo wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
     sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
     sudo apt-get update
-    sudo apt-get install openjdk-7-jdk git jenkins -y
+    sudo apt-get install openjdk-8-jdk git jenkins -y
     sudo groupadd hab && useradd -g hab -s /bin/bash -m hab
+    sudo curl -fsSL https://get.docker.com/ | sh
     sudo usermod -aG docker jenkins
     sudo usermod -aG docker hab
-    sudo curl -fsSL https://get.docker.com/ | sh
     sudo curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
     sudo curl -sSf https://static.rust-lang.org/rustup.sh | sh
 popd
@@ -19,7 +20,9 @@ popd
 # HABITAT
 sudo su -c "mkdir -p /var/lib/jenkins/git/habitat" jenkins
 sudo su -c "git clone https://github.com/habitat-sh/habitat /var/lib/jenkins/git/habitat" jenkins
-sudo su -c "/var/lib/jenkins/git/habitat/components/hab/install.sh" 
+sudo "/var/lib/jenkins/git/habitat/components/hab/install.sh" 
+sudo "/var/lib/jenkins/git/habitat/terraform/scripts/bootstrap.sh" 
+
 
 # JENKINS PLUGINS
 sudo su -c "java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://127.0.0.1:8080 install-plugin git -deploy" jenkins
