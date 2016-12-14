c35f82bba4171503d8b00bbdfb5bedfa7d9722fb
Chirica Gheorghe <chiricagheorghe@gmail.com>
2016-12-14 13:18:58 +0200

    Changed core stack name

--- eu-west-1/core-create.sh
+++ eu-west-1/core-create.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 aws cloudformation create-stack \
-    --stack-name MADCORE-ControlBoxSmall \
+    --stack-name MADCORE-Core \
     --template-body file://core.json \
     --parameters file://core-parameters.json \
     --capabilities "CAPABILITY_IAM"
