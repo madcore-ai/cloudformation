8abc24932d9fba6c137e0d968b140a1ab7b847f7
Chirica Gheorghe <chiricagheorghe@gmail.com>
2017-01-09 15:22:46 +0200

    Removed regions folder, moved to root

--- us-east-1/sgfm-create.sh
+++
@@ -1,4 +0,0 @@
-aws cloudformation create-stack \
-    --stack-name DF-FollowMe \
-    --template-body file://sgfm.json \
-    --parameters file://sgfm-parameters.json 
--- us-east-1/sgfm-update.sh
+++
@@ -1,4 +0,0 @@
-aws cloudformation update-stack \
-    --stack-name DF-FollowMe \
-    --template-body file://sgfm.json \
-    --parameters file://sgfm-parameters.json 
